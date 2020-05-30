5c35060bcaed62775be1ef81c67b5958681b32c4
Le Duy Vu <vlduy2112@gmail.com>
2020-05-29 20:31:29 -0700

    Add function for pseudo half precision

---
+++ add.php.asm
@@ -0,0 +1,16 @@
+.text
+.globl  add.php
+add.php:
+	# input: float  A and B in $f12, $f13
+	# output: php C = A + B in $f0
+	add $sp, $sp, -4
+	sw $ra,	($sp)		# input A is in $f12 
+	jal cvt.php.s		# Convert A to PHP
+	mov.s $f2, $f0		# move A to $f2
+	mov.s $f12, $f13	# move B to cvt.php.s input position
+	jal cvt.php.s		# Convert B to PHP
+	add.s $f12, $f2, $f0	# C = A + B
+	jal cvt.php.s		# convert C to PHP
+	lw $ra,	($sp)
+	add $sp, $sp, 4
+	jr $ra
